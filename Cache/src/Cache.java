4680e06e24793882c3f1d3b1b6535c12314c6520
notmej <yunonaagzamova@gmail.com>
2026-04-21 16:35:44 +0300

    Basic structure done with methods (to be edited)

---
+++ Cache/src/Cache.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author yuna
+ */
+public class Cache {
+    
+    public void directMap(int cpuAddress) {}
+    public void fullyMap(int cpuAddress) {}
+    public void setMap(int cpuAddress) {}
+    public void print() {}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
